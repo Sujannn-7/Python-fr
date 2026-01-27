7c8ddf0e8afdf23c31d01450bb50b3b781e9adfd
Sujan Adhikari <sujan69834@gmail.com>
2026-01-27 20:57:30 +0545

    File I/O

---
+++ file.py
@@ -0,0 +1,6 @@
+fbi = """The FBI(Federal Bureau of Investigation) is the main law enforcement and domestic intelligence agency of the United States. It investigates federal crimes, terrorism, cybercrime, organized crime, and threats to national security within the U.S. The FBI also works to protect civil rights and support state and local law enforcement."""
+
+
+f = open("Sample_file.txt", "w")
+data = f.write(fbi)
+f.close()
---
+++ file0.py
@@ -0,0 +1,4 @@
+f = open("sample_file.txt", "r")
+data = f.read()
+print(data)
+f.close()
