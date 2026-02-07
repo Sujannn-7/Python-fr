8aa9c1cb372ba2a6a7eb684de7afd86caa84de96
Sujan Adhikari <sujan69834@gmail.com>
2026-02-07 21:11:20 +0545

    The car game

---
+++ main1.py
@@ -0,0 +1,28 @@
+command = ""
+started = False
+
+while True:
+    command = input("> ").lower()
+    if command=="start":
+        if started:
+          print("The car is already started")
+        else:
+            started = True
+            print("The car has started")
+    elif command=="stop":
+        if not started:
+           print("The car has already stopped")
+        else:
+           started = False
+           print("The car has stopped")
+    elif command=="help":
+        print("""
+start- to start the car
+stop- to stop the car
+quit - to quit the game
+""")
+    elif command == "quit":
+        print("You quit the game. ")
+        break
+    else:
+       print("You entered invalid input! ")
