bcfb13d4b37e39ab6e8f07ca262591740b969a2b
Sujan Adhikari <sujan69834@gmail.com>
2026-02-20 20:55:40 +0545

    Jarvis 3

--- main3.py
+++ main3.py
@@ -11,20 +11,38 @@ def speak(text):
     engine.runAndWait()
 
 def processCommand(c):
-    print(c)
+    c = c.lower()
+    if "open google" in c:
+        speak("Opening Google now")
+        webbrowser.open("https://google.com")
+    elif "open insta" in c:
+        speak("Opening Instagram now")
+        webbrowser.open("https://instagram.com")
+    elif "open youtube" in c:
+        speak("Opening YouTube now")
+        webbrowser.open("https://youtube.com")
+    elif "open linkedin" in c:
+        speak("Opening LinkedIn now")
+        webbrowser.open("https://linkedin.com")
+    elif "open chatgpt" in c:
+        speak("Opening ChatGPT now")
+        webbrowser.open("https://chatgpt.com")
+    else:
+        speak("Sorry, I didn't understand that command.")
+
+    
 
 if __name__ == "__main__":
     speak("Initialising Jarvis...")
     
     while True:     
-        r = sr.Recognizer()
         print("recognizing")
         try: 
             with sr.Microphone() as source:
-                print("Listening")
+                print("Listening...")
                 recognizer.adjust_for_ambient_noise(source, duration=1)
                 audio = recognizer.listen(source)
-            command = r.recognize_google(audio)
+            command = recognizer.recognize_google(audio)
             if (command.lower() == "jarvis"):
                 speak("Yeah")
 
@@ -33,7 +51,7 @@ if __name__ == "__main__":
                     audio = recognizer.listen(source)
                     command = recognizer.recognize_google(audio)
 
-                    processCommand(command)
+                processCommand(command)
 
         except Exception as e: 
             print("Error; {0}".format(e))
